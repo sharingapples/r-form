444beb98ed947abb1ad5d79a2363ad86e465fa29
Aashish Karki <aashish.y2z@gmail.com>
2018-09-05 11:48:54 +0545

    add domform

--- __test__/form.spec.js
+++ __test__/form.spec.js
@@ -2,18 +2,13 @@ import renderer from 'react-test-renderer';
 import React from 'react';
 import Form from '../src';
 
-
-const inputBox = renderer.create(
-  <form>
-    <Form>
-      <input name="ss" />
-    </Form>
-  </form>,
-);
+function DomForm(value) {
+  return <Form />
+}
 
 describe('Form component Test', () => {
   test('Form Component', () => {
     let tree = inputBox.toJSON();
-    console.log(tree, Form.Input);
+    console.log(tree, InputBox);
   });
 });
