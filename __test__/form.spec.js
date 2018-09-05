92c0ba2b4939d44c3d656da260184e24a6ff2408
Aashish Karki <aashish.y2z@gmail.com>
2018-09-05 11:55:24 +0545

    dom form

--- __test__/form.spec.js
+++ __test__/form.spec.js
@@ -2,13 +2,25 @@ import renderer from 'react-test-renderer';
 import React from 'react';
 import Form from '../src';
 
-function DomForm(value) {
-  return <Form />
+const value = {
+  name: 'Blah', spouse: { name: 'CCC', age: 21 }, hobby: ['Drawing'],
+};
+
+function DomForm(v) {
+  return (
+    <form onSubmit={() => console.log('submit')}>
+      <Form value={v} />
+    </form>
+  );
 }
+
+const app = renderer.create(
+  <DomForm value={value} />,
+);
 
 describe('Form component Test', () => {
   test('Form Component', () => {
-    let tree = inputBox.toJSON();
-    console.log(tree, InputBox);
+    let tree = app.toJSON();
+    console.log(tree);
   });
 });
